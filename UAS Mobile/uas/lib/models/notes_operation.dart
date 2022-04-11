0163a5ca0bc74beebf5e8ebae38256ccd3e9e818
MukhammadSyafii30 <mukhammadsyafii30@gmail.com>
2022-04-11 23:18:13 +0700

    uas mobile

---
+++ UAS Mobile/uas/lib/models/notes_operation.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:uas/models/note.dart';
+import 'package:uas/models/Json_convert.dart';
+
+class NotesOperation with ChangeNotifier {
+  //List of note
+  List<Note> _notes = [];
+
+  List<Note> get getNotes {
+    return _notes;
+  }
+
+  NotesOperation() {
+    addNewNote('First Note', 'First Note Description');
+  }
+
+  void addNewNote(String title, String description) {
+    //Note object
+    Note note = Note(title, description);
+    _notes.add(note);
+    notifyListeners();
+  }
+}
